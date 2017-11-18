5b702e6a388a0cda6cea5a009ab742a295aa1dc7
Yury Belousov <yury-belousov@yandex.ru>
2017-11-18 20:55:06 +0300

    add button

--- MobileApp/MobileApp/MobileApp/MainPage.xaml.cs
+++ MobileApp/MobileApp/MobileApp/MainPage.xaml.cs
@@ -9,9 +9,17 @@ namespace MobileApp
 {
 	public partial class MainPage : ContentPage
 	{
+        private static int _country = 0;
 		public MainPage()
 		{
 			InitializeComponent();
 		}
+
+	    private void Button_OnClicked(object sender, EventArgs e)
+	    {
+	        Device.OpenUri(_country++ % 2 == 0
+	            ? new Uri("https://open.spotify.com/user/spotifycharts/playlist/37i9dQZEVXbLRQDuF5jeBp")
+	            : new Uri("https://open.spotify.com/user/spotifycharts/playlist/37i9dQZEVXbJiZcmkrIHGU"));
+	    }
 	}
 }
