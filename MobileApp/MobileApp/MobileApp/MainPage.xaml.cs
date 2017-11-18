9bed35265ce11d0a9bbad5a10f6d807797e69a30
Yury Belousov <yury-belousov@yandex.ru>
2017-11-18 22:25:04 +0300

    add background image

--- MobileApp/MobileApp/MobileApp/MainPage.xaml.cs
+++ MobileApp/MobileApp/MobileApp/MainPage.xaml.cs
@@ -7,19 +7,28 @@ using Xamarin.Forms;
 
 namespace MobileApp
 {
-	public partial class MainPage : ContentPage
-	{
-        private static int _country = 0;
-		public MainPage()
-		{
-			InitializeComponent();
-		}
+    public partial class MainPage : ContentPage
+    {
+        private static int _country;
 
-	    private void Button_OnClicked(object sender, EventArgs e)
-	    {
-	        Device.OpenUri(_country++ % 2 == 0
-	            ? new Uri("https://open.spotify.com/user/spotifycharts/playlist/37i9dQZEVXbLRQDuF5jeBp")
-	            : new Uri("https://open.spotify.com/user/spotifycharts/playlist/37i9dQZEVXbJiZcmkrIHGU"));
-	    }
-	}
+        private const string BaseUrl = "https://open.spotify.com/user/spotifycharts/playlist/";
+        private static readonly Dictionary<string, string> PopularChartsInCountry = new Dictionary<string, string>
+        {
+            ["us"] = "37i9dQZEVXbLRQDuF5jeBp",
+            ["de"] = "37i9dQZEVXbJiZcmkrIHGU"
+
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Device.OpenUri(_country++ % 2 == 0
+                ? new Uri(BaseUrl + PopularChartsInCountry["en"])
+                : new Uri(BaseUrl + PopularChartsInCountry["de"]));
+        }
+    }
 }
